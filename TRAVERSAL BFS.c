ca10c9dfcf3f5d31007c3e33d30f3aa25e43e167
ShanthiRani05 <123440133+ShanthiRani05@users.noreply.github.com>
2023-08-29 14:40:53 +0530

    Add files via upload

---
+++ TRAVERSAL BFS.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure to represent a node in the adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Structure to represent the adjacency list for each vertex
+struct Graph {
+    int V; // Number of vertices
+    struct Node** adjList; // Array of linked lists
+};
+
+// Function to create a new node
+struct Node* createNode(int vertex) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = vertex;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with V vertices
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->adjList = (struct Node**)malloc(V * sizeof(struct Node*));
+
+     (int i = 0; i < V; i++) {
+        graph->adjList[i] = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add an edge to an undirected graph
+void addEdge(struct Graph* graph, int src, int dest) {
+    // Add an edge from src to dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjList[src];
+    graph->adjList[src] = newNode;
+
+    // Add an edge from dest to src
+    newNode = createNode(src);
+    newNode->next = graph->adjList[dest];
+    graph->adjList[dest] = newNode;
+}
+
+// Function to print the adjacency list of the graph
+void printGraph(struct Graph* graph) {
+    for (int v = 0; v < graph->V; v++) {
+        struct Node* temp = graph->adjList[v];
+        printf("Adjacency list of vertex %d: ", v);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int V = 5; // Number of vertices
+    struct Graph* graph = createGraph(V);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+
+    printf("Bidirectional Graph:\n");
+    printGraph(graph);
+
+    return 0;
+}
+
